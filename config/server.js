516021477e60f747f65ef6a056978d115c38c121
Rapee Suveeranont <iiieyes@gmail.com>
2020-12-10 15:34:13 +0700

    Add Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '53108c411b226947164170ab96e74c72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '122bb7c2-e597-43bc-8402-05e737988b2e'
+};
