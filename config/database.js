5d3870b58ff3454f98ab6831c53dd29565a5333c
Rapee Suveeranont <iiieyes@gmail.com>
2020-12-11 17:33:29 +0700

    Update database to support SQLite3

--- config/database.js
+++ config/database.js
@@ -2,18 +2,13 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'electinth-constitution'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'sukhumvit41'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', false),
+        useNullAsDefault: true,
       },
     },
   },
